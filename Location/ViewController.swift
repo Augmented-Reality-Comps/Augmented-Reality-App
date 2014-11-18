8a156ff86bc713e8ddd9297eaec2ffa8c5529793
comps <comps@cmc307-08.mathcs.carleton.edu>
2014-11-18 10:35:00 -0600

    Commit before end of fall term

--- Location/ViewController.swift
+++ Location/ViewController.swift
@@ -11,10 +11,19 @@
 
 import UIKit
 import CoreLocation
+import CoreMotion
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var pitch: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var Latitude: UILabel!
+    @IBOutlet weak var roll: UILabel!
+    @IBOutlet weak var yaw: UILabel!
+    
     let locationManager = CLLocationManager()
+    let motionManager = CMMotionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +33,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
         
+        self.motionManager.deviceMotionUpdateInterval = 0.1
+        self.motionManager.startDeviceMotionUpdates()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
@@ -37,14 +47,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func displayLocationInfo(location: CLLocation) {
         
-        self.locationManager.stopUpdatingLocation()
-        println("Latitude: ", location.coordinate.latitude)
+        if let attitude = motionManager.deviceMotion?.attitude? {
+              self.pitch.text = "Pitch: " + String(format: "%i", Int(motionManager.deviceMotion.attitude.pitch*57.2957795131)) + "°"
+            self.roll.text = "Roll: " + String(format: "%i", Int(motionManager.deviceMotion.attitude.roll*57.2957795131)) + "°"
+            self.yaw.text = "Yaw: " + String(format: "%i", Int(motionManager.deviceMotion.attitude.yaw*57.2957795131)) + "°"
+        }
+      
+        self.Latitude.text = "Latitude: " + String(format: "%f", location.coordinate.latitude )
+        self.longitude.text = "Longitude: " + String(format: "%f", location.coordinate.longitude)
+        self.altitude.text = "Altitude: " + String(format: "%f", location.altitude)
+
+
+       /* println("Latitude: ", location.coordinate.latitude)
         println("Longitude: ", location.coordinate.longitude)
         println("Altitude: ", location.altitude)
         println("Horizontal Accuracy: ", location.horizontalAccuracy)
         println("Vertical Accuracy: ", location.verticalAccuracy)
         println("Timestamp: ", location.timestamp)
         println("Description: ", location.description)
+*/
         
     }
     
